651a73e33f72025b9de422e6ffaf03f50fd06d04
shout64 <135728867+shout64@users.noreply.github.com>
2023-06-06 11:39:51 -0500

    Full scripts
    Updated my local copies to reflect all of the fields used in production

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import openpyxl as ox
 
 # Read CSV, update column names and sort by Name
 df = pd.read_csv(r'C:\members.csv')
-df = df.rename(columns={'displayName': 'Name', 'mail': 'Email', 'telephoneNumber': 'Phone Number'})
+df = df.rename(columns={'displayName': 'Name', 'title': 'Title', 'department': 'Department', 'physicalDeliveryOfficeName': 'Office Location', 'mail': 'Email', 'telephoneNumber': 'Phone Number'})
 df = df.sort_values('Name')
 
 # Create Excel Doc
@@ -13,7 +13,10 @@ df.to_excel(r'C:\Contact List.xlsx', sheet_name='Contacts', index=False)
 workbook = ox.load_workbook(r'C:\Contact List.xlsx')
 worksheet = workbook['Contacts']
 worksheet.column_dimensions['A'].width = 25
-worksheet.column_dimensions['B'].width = 40
-worksheet.column_dimensions['C'].width = 16
+worksheet.column_dimensions['B'].width = 65
+worksheet.column_dimensions['C'].width = 48
+worksheet.column_dimensions['D'].width = 40
+worksheet.column_dimensions['E'].width = 40
+worksheet.column_dimensions['F'].width = 20
 
 workbook.save(r'C:\Contact List.xlsx')
